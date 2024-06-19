5e3cac3b2fd448acd6f7f70e5031056f5defa033
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-19 21:01:31 +0200

    used States to access the text and the aamount

--- src/components/AddTransanction.js
+++ src/components/AddTransanction.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 const AddTransanction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
   return (
     <div className="AddTransaction">
       <h3>Add new Transaction</h3>
@@ -9,7 +11,12 @@ const AddTransanction = () => {
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <br />
-          <input type="text" className="input-text" placeholder="Enter text" />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Enter text"
+            onChange={(e) => setText(e.target.value)}
+          />
           <div className="form-control">
             <label htmlFor="amount">
               Amount
@@ -21,8 +28,10 @@ const AddTransanction = () => {
               type="number"
               className="enter-amount"
               placeholder="Enter amount"
+              onChange={(e) => setAmount(e.target.value)}
             />
           </div>
+          <button className="transaction-btn">Add Transaction</button>
         </div>
       </form>
     </div>
