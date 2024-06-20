b679adc4d4ab0f2b3893fc3d138d011eb82c3785
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-20 10:12:55 +0200

    calculated income and expense values

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { GlobalContext } from "../context/GlobalState";
 
 const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense =
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2);
   return (
     <div className="IncomeExpense-container">
       <div className="row">
@@ -9,8 +19,8 @@ const IncomeExpense = () => {
         <div className="col-6">Expense</div>
       </div>
       <div className="row">
-        <div className="col-6">+$0.00</div>
-        <div className="col-6">-$0.00</div>
+        <div className="col-6">${income}</div>
+        <div className="col-6">${expense}</div>
       </div>
     </div>
   );
